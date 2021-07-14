4e9171456fb01f5c967154b681ab358f568e27f2
Antwan37 <70380738+Antwan37@users.noreply.github.com>
2021-07-14 21:38:10 +0200

    Update babele-register.js

--- babele-register.js
+++ babele-register.js
@@ -1,1 +1,9 @@
-
+Hooks.on('init', () => {
+    if(typeof Babele !== 'undefined') {
+        Babele.get().register({
+            module: 'tftloop-fr-translation',
+            lang: 'fr',
+            dir: 'compendium'
+        });
+    }
+});
