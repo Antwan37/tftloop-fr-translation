188c5501e146a84a19e6976de61b8c3fba1cd3d2
Antwan37 <70380738+Antwan37@users.noreply.github.com>
2021-07-20 19:10:13 +0200

    Update register-babele.js

--- register-babele.js
+++ register-babele.js
@@ -1,7 +1,7 @@
 Hooks.on('init', () => {
     if(typeof Babele !== 'undefined') {
         Babele.get().register({
-            module: 'systemefoundry-fr-translation',
+            module: 'tftloop-fr-translation',
             lang: 'fr',
             dir: 'compendium'
         });
